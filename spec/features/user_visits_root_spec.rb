ed8f7f4f1b841ca4e9b2f28fc68039e7eeddb4f6
Dan Hutch <danhutch5280@gmail.com>
2019-02-24 15:43:39 -0700

    Implement attribution footer

--- spec/features/user_visits_root_spec.rb
+++ spec/features/user_visits_root_spec.rb
@@ -6,7 +6,6 @@ describe 'Visitor' do
       VCR.use_cassette("newsapi_service_spec") do
 
         visit root_path
-save_and_open_page
         expect(page).to have_css('.news-feed-main')
         expect(page).to have_css('.story-card', count: 20)
 
@@ -17,6 +16,20 @@ save_and_open_page
           expect(page).to have_css('.by-line')
           expect(page).to have_css('.published-at')
           expect(page).to have_css('.description')
+        end
+      end
+    end
+
+    it 'can see the attribution footer' do
+      VCR.use_cassette("newsapi_service_spec") do
+        visit root_path
+
+        expect(page).to have_css('.main-footer')
+
+        within('.main-footer') do
+          expect(page).to have_css('.attribution')
+          expect(page).to have_content('Powered By NewsAPI.org')
+          expect(page).to have_link('NewsAPI.org')
         end
       end
     end
