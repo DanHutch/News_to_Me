c41bee7d5005c0d2310fb299e966d9afc61b9bd2
Dan Hutch <danhutch5280@gmail.com>
2019-02-23 21:36:49 -0700

    Implement VCR for service spec

--- spec/services/newsapi_service_spec.rb
+++ spec/services/newsapi_service_spec.rb
@@ -2,12 +2,27 @@ require 'rails_helper'
 
 describe NewsApiService do
 
-  it "exists" do
-    VCR.use_cassette("newsapi_service_spec") do
-      service = NewsApiService.get_news('bitcoin')
-      expect(service).to be_a(NewsApiService)
-    end
-  end
+  context "class methods" do
 
+    context ".get_news" do
+      it "returns a user's data" do
+        VCR.use_cassette("newsapi_service_spec") do
+          news = NewsApiService.get_news('bitcoin')
+          expect(news).to be_a(Array)
+          expect(news.length).to eq(20)
+          news.each do |story|
+            expect(story).to respond_to(:name)
+            expect(story).to respond_to(:author)
+            expect(story).to respond_to(:title)
+            expect(story).to respond_to(:description)
+            expect(story).to respond_to(:url)
+            expect(story).to respond_to(:urlToImage)
+            expect(story).to respond_to(:publishedAt)
+          end
+        end
+      end
+    end
+
+  end
 
 end
